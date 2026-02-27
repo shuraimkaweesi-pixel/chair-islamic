263c30ed37bbe3ea4eeacb4c61a9a39b957c28cb
shuraimkaweesi-pixel <shuraimkaweesi@gmail.com>
2026-02-28 02:01:53 +0300

    Update chat.js
    Signed-off-by: shuraimkaweesi-pixel <shuraimkaweesi@gmail.com>

--- api/chat.js
+++ api/chat.js
@@ -21,10 +21,11 @@ export default async function handler(req, res) {
       temperature: 0.5
     });
 
-    const aiAnswer = response.choices[0].message.content;
-    res.status(200).json({ answer: aiAnswer });
+    const answer = response.choices[0].message.content;
+    res.status(200).json({ answer });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "OpenAI request failed." });
   }
 }
+Add AI backend function chat.js
