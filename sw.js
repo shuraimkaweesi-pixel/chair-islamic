0ba1fc1efbf8486523f15f023a09ef0a6e37a646
shuraimkaweesi-pixel <shuraimkaweesi@gmail.com>
2026-03-21 12:24:36 +0300

    Update sw.js
    Signed-off-by: shuraimkaweesi-pixel <shuraimkaweesi@gmail.com>

--- sw.js
+++ sw.js
@@ -1,5 +1,27 @@
-self.addEventListener("install", e => {
-  console.log("Service Worker Installed");
+const CACHE_NAME = "chair-tv-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+  "/logo.png"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch", e => {});
+// Fetch (offline support)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
